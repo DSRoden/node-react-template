56b8a29a12645896a5e08afaa67e5a6de34be7b6
Daniel <daniel.s.roden@gmail.com>
2016-06-16 11:00:50 +0200

    server

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -29,34 +29,6 @@ module.exports = function(app, express){
 
   // app.use(security.authenticate);
   app.use(debug.info);
-  app.post('/register', users.register);
-  app.post('/login', users.login);
-
-  app.use(security.bounce);
-  app.post('/cosmosCreate', cosmos.createOne);
-  app.post('/allMyCosmos', cosmos.getAllPersonal);
-  app.post('/getOneCosmos', cosmos.getOne);
-  // app.post('/deleteCosmos', cosmos.deleteOne);
-
-  app.post('/createSpace', spaces.createOne);
-  app.post('/getOneSpace', spaces.getOne);
-  // app.post('/allSpaces', space.getAll);
-  // app.post('/deleteSpace', space.deleteOne);
-
-  // app.post('/savenode', nodes.saveAxons);
-  app.post('/saveMap', maps.save);
-  app.get('/retrieveMap', maps.retrieve);
-
-  app.post('/saveNode', nodes.saveOne);
-  app.post('/deleteNode', nodes.deleteOne);
-
-  app.post('/saveAxon', axons.saveOne);
-  app.post('/getOneAxon', axons.getOneAxon);
-  app.post('/deleteAxon', axons.deleteOne);
-  app.get('/allnodes', nodes.retrieveAxons);
-
-  app.post('/upload',  multipartyMiddleware, files.upload);
-  app.get('/logout', users.logout);
   //console.log('Express: Routes Loaded');
 };
 
