55e93841e08f2c650972c67a6c4ce6a3160cce4e
Daniel <daniel.s.roden@gmail.com>
2016-06-16 05:20:10 +0200

    address

--- client/www/js/bundle.js
+++ client/www/js/bundle.js
@@ -37699,7 +37699,8 @@ module.exports = router;
 var AppConstants = require("./constants");
 
 module.exports.getHostName = function () {
-    return "http" + (AppConstants.DEBUG ? "" : "s") + "://" + AppConstants.HOST;
+    // return "http" + (AppConstants.DEBUG ? "" : "s") + "://" + AppConstants.HOST;
+    return "http://ubuntu@54.235.229.113";
 };
 
 module.exports.getPath = function (path) {
--- client/www/js/utils.js
+++ client/www/js/utils.js
@@ -1,7 +1,9 @@
 var AppConstants = require("./constants");
 
 module.exports.getHostName = function() {
-    return "http" + (AppConstants.DEBUG ? "" : "s") + "://" + AppConstants.HOST;
+    // return "http" + (AppConstants.DEBUG ? "" : "s") + "://" + AppConstants.HOST;
+    return "http://ubuntu@54.235.229.113";
+
 }
 
 module.exports.getPath = function(path) {
